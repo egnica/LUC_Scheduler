246b09e0a71a7939dccba0ab06b6435fca0acab3
Nicholas Egner <nick@nicholasegner.com>
2026-08-18 15:20:47 -0500

    commit

--- app/api/google/callback/route.js
+++ app/api/google/callback/route.js
@@ -14,6 +14,10 @@ export async function GET(request) {
 
   const { tokens } = await googleOAuth2Client.getToken(code);
 
+  console.log("NEW GOOGLE REFRESH TOKEN:", tokens.refresh_token);
+
+  googleOAuth2Client.setCredentials(tokens);
+
   googleOAuth2Client.setCredentials(tokens);
 
   const calendar = google.calendar({
